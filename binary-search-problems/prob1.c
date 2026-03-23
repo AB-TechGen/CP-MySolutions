b6728eeb7badf498d3939e8657dafcdac3034c90
Aarush <abtechgen07@gmail.com>
2026-03-23 14:39:08 +0530

    binary search prob 1

---
+++ binary-search-problems/prob1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void correctSol(int i) {
+    printf("First true index is at %d", i);
+}
+
+void wrongSol() {
+    printf("No valid solution!\n");
+}
+
+int binarySearch(int* A, int n) {
+    int l = 0, r = n-1;
+    int first_true_index = -1;
+    while (l <= r) {
+        int mid = l + (r - l)/2;
+        if (A[mid]) {
+            first_true_index = mid;
+            r = mid - 1;
+        }
+        else l = mid + 1;
+    }
+    return first_true_index;
+}
+
+int main() {
+    int A[] = {0, 0, 1, 1, 1};
+    int n = sizeof(A)/sizeof(int);
+    int answer = binarySearch(A, n);
+    if (answer == -1)  wrongSol();
+    else correctSol(answer);
+    return 0;
+}
